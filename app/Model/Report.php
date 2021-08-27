cc6050af720766d6e102c360a7d7fc21cd1b99d0
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2021-08-27 06:24:12 -0700

    report relationships

--- app/Model/Report.php
+++ app/Model/Report.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+   public function expense()
+   {
+      return $this->belongsTo(Expense::class,'expense_id');
+   }
+   public function income()
+   {
+      return $this->belongsTo(Income::class,'income_id');
+   }
+   public function borrow()
+   {
+      return $this->belongsTo(Borrow::class,'borrow_id');
+   }
+   
 }
